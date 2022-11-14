13aef6d5099d340f8b44498ba4a09e8aad6840f9
Ali Mohammadi <ali_mohammadi@ut.ac.ir>
2022-11-14 23:59:52 +0330

    Add movie update url to urls file

--- movies/urls.py
+++ movies/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from movies.views import movies_list, movie_detail
+from movies.views import movies_list, movie_detail, movie_update
 
 urlpatterns = [
     path('', movies_list, name='movies_list'),
     path('movies/', movies_list, name='movies_list'),
     path('movie/<int:pk>', movie_detail, name='movie_detail'),
+    path('movies/<int:pk>/edit/', movie_update, name='movie_update'),
 ]
