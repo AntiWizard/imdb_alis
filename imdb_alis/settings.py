8e56978992713d0d913f323aeec3102d2ffc6099
aliminuei@gmail.com <aliminuei@gmail.com>
2022-11-21 20:41:15 +0330

    REFACTOR templates base and style css

--- imdb_alis/settings.py
+++ imdb_alis/settings.py
@@ -14,9 +14,9 @@ from pathlib import Path
 
 from decouple import config, Csv
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
-
 import movies.apps
+
+# Build paths inside the project like this: BASE_DIR / 'subdir'.
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -126,7 +126,12 @@ STATIC_URL = 'static/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 MEDIA_URL = 'media/'
 
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = "users.CustomUser"
